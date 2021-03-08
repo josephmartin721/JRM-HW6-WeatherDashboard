5eea6406afe11bac7366bbd186bab03b3ef35da9
Joseph Martin <josephmartin721@yahoo.com>
2021-03-08 11:21:00 -0600

    added search function to script

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //function getResults(resultObj) {}
 // weather api :  https://api.weather.gov/points/39.7456,-97.0892
 
+$(document).ready(function () {
+    $("#search-button").on("click", function () {
+        var searchTerm = $("#search-value").val();
+        $("#search-value").val("");
+        weatherFunction(searchTerm);
+        weatherForecast(searchTerm);
+    });
