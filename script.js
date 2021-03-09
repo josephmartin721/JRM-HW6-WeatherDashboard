29ec0e7e092fbfb0fb683e07c3e31b3a310250c7
Joseph Martin <josephmartin721@yahoo.com>
2021-03-08 21:38:20 -0600

    issues continue

--- script.js
+++ script.js
@@ -29,10 +29,31 @@ var history = JSON.parse(localStorage.getItem("history")) || [];
     });
 
     function weatherFunction(searchTerm) {
-  
+  //constant semantic error
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=f467738ba5027fc3bee62c611bd30bce&units=imperial",
         method: "GET",
     }).then(function (data) {
         console.log(data);
         $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+        for (var i = 0; i < data.list.length; i++) {
+
+            if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+                var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+
+                var colFive = $("<div>").addClass("col-md-2");
+                var cardFive = $("<div>").addClass("card bg-primary text-white");
+                var cardBodyFive = $("<div>").addClass("card-body p-2");
+                var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+
+                
+                colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, tempFive)));
+                $("#forecast .row").append(colFive);
+            }
+        }
+    });
+}
+});
